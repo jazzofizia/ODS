c9d2c80ad7a0606a787116f66ed590c72ce489ba
Jawad KHan <jawad-ali@live.com>
2019-01-20 16:01:08 +0500

    Add files via upload

---
+++ Lab#17-Inline Function/Inline-done.cpp
@@ -0,0 +1,24 @@
+// [](){} notation  //  bsm annotaiton
+//lambda expressions only available with -std=c++11 or -std=gnu++11
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  vector<int> vec(10);
+  // output initial value of each element
+  // for_each(vec.begin(), vec.end(), print); ==>
+  for_each(vec.begin(), vec.end(), [](int v) {cout << v << " ";});
+  cout << endl;
+
+  // assign a value to each element of a vector
+  // for_each(vec.begin(), vec.end(), assign); ==>
+  for_each(vec.begin(), vec.end(), [](int& v) {static int n = 1; v = n++;});
+
+  // output updated value of each element
+  // for_each(vec.begin(), vec.end(), print); ==>
+  for_each(vec.begin(), vec.end(), [](int v) {cout << v << " ";});
+  return 0;
+}
