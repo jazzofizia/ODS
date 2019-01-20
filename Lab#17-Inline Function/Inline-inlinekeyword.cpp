60b03dcb7f4853d25e8b2b08201b2cb3c3287430
Jawad KHan <jawad-ali@live.com>
2019-01-20 17:00:11 +0500

    Add files via upload

---
+++ Lab#17-Inline Function/Inline-inlinekeyword.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// assign a value to each element of a vector
+inline void assign(int& v)  
+{
+   static int n = 1; v = n++;
+}
+
+// print out each element
+inline void print(int v)  
+{
+  cout << v << " ";
+}
+
+int main()
+{
+	//with each iteration, a fucntionis called which makes the program jumpy and vibratory. (takes less memory)
+	// simple memory: Output Size: 1.8488130569458 MiB
+	//inline memory: Output Size: 1.84916877746582 MiB (300)
+  vector<int> vec(10);
+  // output initial value of each element
+  for_each(vec.begin(), vec.end(), print); //the function print takes value from first to last of vector vec (algorthm package usage)
+  cout << endl;
+
+  // assign a value to each element of a vector
+  for_each(vec.begin(), vec.end(), assign);
+
+  // output updated value of each element
+  for_each(vec.begin(), vec.end(), print);
+  return 0;
+}
